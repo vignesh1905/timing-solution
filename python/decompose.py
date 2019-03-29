8a117113e7bbc2fee5e3cd7e8320507f4498974c
vignesh1905 <vigneshkalai@outlook.com>
2019-03-29 22:46:54 +0800

    Adding recompose script and ffmpeg commands

--- python/decompose.py
+++ python/decompose.py
@@ -1,9 +1,9 @@
 import cv2
-vidcap = cv2.VideoCapture('/Users/vigneshk/Desktop/timing_project/videos/IMG_0004.mp4')
+vidcap = cv2.VideoCapture('/Users/vigneshk/Desktop/timing_solution/videos/IMG_0004.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-     cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+     cv2.imwrite("/Volumes/G-DRIVE mobile USB-C/timing_solution/video_frames/frame%d.jpg" % count, image)     # save frame as JPEG file
      success,image = vidcap.read()
      print('Read a new frame: ', success)
      count += 1
---
+++ python/recompose.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+
+image_folder = '/Volumes/G-DRIVE mobile USB-C/timing_solution/video_frames'
+video_name = 'recomposed.mp4'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
